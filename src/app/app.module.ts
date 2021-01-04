2c59e382685c865afe13e162a757fd5e58f98078
Shantanu Khandve <shantanukhandve209@gmail.com>
2021-01-04 13:32:17 +0530

    btagain

---
+++ src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'; // this is needed!
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app.routing';
+import { SectionsModule } from './sections/sections.module';
+import { ComponentsModule } from './components/components.module';
+import { ExamplesModule } from './examples/examples.module';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+
+import { PresentationModule } from './presentation/presentation.module';
+
+import { MglTimelineModule } from 'angular-mgl-timeline';
+ 
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        NavbarComponent
+    ],
+    imports: [
+        BrowserAnimationsModule,
+        NgbModule,
+        FormsModule,
+        RouterModule,
+        AppRoutingModule,
+        PresentationModule,
+        SectionsModule,
+        ComponentsModule,
+        BrowserAnimationsModule,
+        MglTimelineModule,
+        ExamplesModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/sections/features/features.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-features',
+  templateUrl: './features.component.html',
+  styleUrls: ['./features.component.scss']
+})
+export class FeaturesComponent implements OnInit {
+  name;
+
+  constructor() { }
+
+  ngOnInit() {
+
+  } 
+  submit(){
+    console.log(this.name)
+  }
+}
